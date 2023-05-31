c0007e7cd727da52aa51980ac9c3d7dcd8a69470
PK19 <kezmin2016@ya.ru>
2023-05-31 23:44:18 +0300

    fix

--- realestate/settings.py
+++ realestate/settings.py
@@ -53,9 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'realestate.urls'
